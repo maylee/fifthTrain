25b2faa0633a3fca72c0e4eececbef7a4530adc3
cio <cio@mitlab.kr>
2019-05-07 22:09:37 +0900

    Build RESTfull API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,9 +1,47 @@
 const express = require("express");
 const router = express.Router();
 
+// get
 router.get("/", (req, res) => {
   res.status(200).json({
       ord_msg: "GET /orders"
   });
 });
+
+// get with parameter
+router.get('/:orderId',(req,res) => {
+  const id = req.params.orderId;
+  if( id !== '123'){
+      res.status(200).json({
+          ord_msg: "Wrong ID"
+      });
+  } else {
+      res.status(200).json({
+        ord_msg: "Correct ID"
+      });
+  };
+});
+
+
+// post
+router.post('/',(req,res) => {
+  res.status(200).json({
+    ord_msg:"POST / orders"
+  });
+});
+
+// patch
+router.patch('/',(req,res) => {
+  res.status(200).json({
+    ord_msg:"PATCH / orders"
+  });
+});
+
+// delete
+router.delete('/',(req,res) => {
+  res.status(200).json({
+    ord_msg:"DELETE / orders"
+  });
+});
+
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router() ;
 
+// get
 router.get('/',(req,res)=> {
     res.status(200).json({
         prd_msg: "GET /products"
+    });
+});
+
+// get with parameter
+router.get('/:productId',(req,res) => {
+    if (req.params.productId === 'special') {
+        res.status(200).json({
+            prd_msg:"GET / special product"
+        });
+    } else {
+        res.status(200).json({
+            prd_msg:"GET / normal product"
+        });
+    }
+});
+
+// post
+router.post('/', (req, res) => {
+    res.status(200).json({
+        prd_msg:"POST / proudcts"
+    });
+});
+
+// patch
+router.patch('/', (req,res) => {
+    res.status(200).json({
+        prd_msg:"PATCH / products"
+    });
+});
+
+// delete
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        prd_msg:"DELETE / products"
     });
 });
 
