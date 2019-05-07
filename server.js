df3db4884578cfb7c8be601d3b74df85600e7a1e
cio <cio@mitlab.kr>
2019-05-07 22:53:17 +0900

    Error Handling

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
-// Budild Basic Web server
 const http = require('http');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const orderRoutes = require('./api/routes/orders');
 const productRoutes = require('./api/routes/products');
+
+app.use(morgan('dev'));
 app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
+
+app.get('/',(req, res) => {
+    res.send('Root');
+});
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message : error.message
+        }        
+    });
+});
 
 const PORT = 3000;
 const server = http.createServer(app);
