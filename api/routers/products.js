fbdbe00e3948847d1b2cca8760e6e9e01f92962c
cio <cio@mitlab.kr>
2019-05-10 21:16:46 +0900

    POST Product Info to DB

--- api/routers/products.js
+++ api/routers/products.js
@@ -23,17 +23,32 @@ router.get('/', (req, res) => {
       });
   });
 
-// post
-router.post('/', (req, res) => {
-    const productInfo = {
+// POST Product Info to DB
+router.post('/', (req,res) =>{
+    const product = new productModel({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        prd_msg:"POST / proudcts",
-        productInfo: productInfo
     });
+
+    product
+        .save()
+        .then( result => {
+            console.log(result);
+            res.status(200).json({
+                prd_msg: "POST prudctInfo OK",
+                createProduct: result
+            });
+        })
+        .catch( err => {
+            console.log(err);
+            res.status(500).json({
+                prd_err: err
+            });
+        });
 });
+
+
 
 // patch
 router.patch('/', (req,res) => {
