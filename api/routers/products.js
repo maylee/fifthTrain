db8579bae85a0f62461cbbac23f7720d24a5e99b
cio <cio@mitlab.kr>
2019-05-10 21:36:10 +0900

    Get Product Info through productId

--- api/routers/products.js
+++ api/routers/products.js
@@ -23,6 +23,31 @@ router.get('/', (req, res) => {
       });
   });
 
+// Get Product Info through productId
+router.get('/:productId', (req,res) => {
+    const id = req.params.productId;
+    productModel.findById(id)
+        .exec()
+        .then( doc => {
+            console.log("Quering DB...", doc);
+            if (doc) {
+                res.status(200).json({
+                    product_Info: doc
+                });
+            } else {
+                res.status(400).json({
+                    prd_msg: "해당정보 없음"
+                });
+            }
+        })
+        .catch( err => {
+            console.log(err);
+            res.status(500).json({
+                prd_err: err
+            });
+        });
+});
+
 // POST Product Info to DB
 router.post('/', (req,res) =>{
     const product = new productModel({
