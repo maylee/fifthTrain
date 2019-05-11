61fe3a77b5653cf8682a0e7d00a919701b0f8c41
cio <cio@mitlab.kr>
2019-05-11 20:01:31 +0900

    Register Order Info - POST

--- api/routers/orders.js
+++ api/routers/orders.js
@@ -26,12 +26,41 @@ router.get('/:orderId',(req,res) => {
   };
 });
 
-// post
-router.post('/',(req,res) => {
-  res.status(200).json({
-    ord_msg:"POST / orders"
+// Register Order Info - POST
+router.post('/', (req, res) => {
+  productModel.findById(req.body.productId)
+  .then( product => {
+    if(!product){
+      return res.status(404).json({
+        prd_err: "해당상품 정보를 찾지 못했습니다."
+      });
+    }
+    const order = new orderModel({
+      _id: mongoose.Types.ObjectId(),
+      quantity: req.body.quantity,
+      product : req.body.productId
+    });
+    return order.save();
+  })
+  .then( result => {
+    console.log(result);
+    res.status(201).json({
+      prd_msg: "주문이 처리 되었습니다.",
+      createdOrder: {
+        _id: result._id,
+        product: result.prodcut,
+        quantity: result.quantity
+      }
+    });
+  })
+  .catch( err => {
+    console.log(err);
+    res.status(500).json({
+      prd_err: err
+    });
   });
 });
+
 
 // patch
 router.patch('/',(req,res) => {
